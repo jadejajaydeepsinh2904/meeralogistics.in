7f649dd6a0944073a40be691630c056a606e7411
jadejajaydeepsinh2904 <jadejajaydeepsinhk007@gmail.com>
2026-05-07 14:05:32 +0530

    Add RootLayout component with metadata for app

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Meera Logistics",
+  description: "Transport Services Gujarat",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
