b3002d2106f2d182c1c73eb8132688301a4fe76d
jadejajaydeepsinh2904 <jadejajaydeepsinhk007@gmail.com>
2026-05-07 15:44:57 +0530

    Update layout metadata and body style
    Updated metadata with new title and description. Modified body style to remove margin.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,12 +1,13 @@
 export const metadata = {
-  title: "Meera Logistics",
-  description: "Transport Services Gujarat",
+  title: "Meera Logistics | Tipper Dumper Truck Transport Gujarat",
+  description:
+    "Meera Logistics provides tipper, dumper and truck transport service all over Gujarat from Jamnagar.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body style={{ margin: 0 }}>{children}</body>
     </html>
   );
 }
